91b4937f0e54ad54d3e1611a43052c9716d35388
Somnath Haldar <TechSom93>
2024-04-04 02:43:50 +0530

    all slider fixed for college page

--- src/utils/Constants/custom-components/CustomSwiper.js
+++ src/utils/Constants/custom-components/CustomSwiper.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {register} from "swiper/element/bundle"
-import { Navigation } from 'swiper/modules';
 
 const responsive = 
   JSON.stringify({
@@ -26,24 +25,14 @@ const responsive =
     }
   })
 
-
-  // const params = {
-  //   modules: [Navigation],
-  //   // inject modules styles to shadow DOM
-  //   injectStylesUrls: [
-  //     'path/to/navigation-element.min.css',
-  //   ],
-  // };
-  // Object.assign(params);
-
-export default function CustomSwiper({noOfSlidesPerView, children, isBreakPoint, navigation}) {
+export default function CustomSwiper({noOfSlidesPerView, children, isBreakPoint}) {
     register();
   return (
     <>
     {/* {isBreakPoint ? (<swiper-container slides-per-view={noOfSlidesPerView} space-between={20} navigation={true} breakpoints={responsive}>{children}</swiper-container>) : 
     <swiper-container slides-per-view={noOfSlidesPerView} space-between={20} navigation={true}>{children}</swiper-container>} */}
-    {isBreakPoint ? (<swiper-container slides-per-view={noOfSlidesPerView} slidesPerView={'auto'} space-between={20} navigation={navigation} breakpoints={responsive}>{children}</swiper-container>) : 
-    <swiper-container slides-per-view={noOfSlidesPerView} slidesPerView={'auto'} space-between={20} navigation={navigation}>{children}</swiper-container>}
+    {isBreakPoint ? (<swiper-container slides-per-view={noOfSlidesPerView} slidesPerView={'auto'} space-between={20} navigation={true} breakpoints={responsive}>{children}</swiper-container>) : 
+    <swiper-container slides-per-view={noOfSlidesPerView} slidesPerView={'auto'} space-between={20} navigation={true}>{children}</swiper-container>}
         
     </>
   )
