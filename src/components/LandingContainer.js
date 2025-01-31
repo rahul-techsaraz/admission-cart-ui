47f22e04e8b59f1b328324afb69a8971c86ab70e
Adarsh-MERN <singhadarsh999@gmail.com>
2025-01-31 16:24:29 +0530

    chat bot, popup, move to top implemented

--- src/components/LandingContainer.js
+++ src/components/LandingContainer.js
@@ -6,9 +6,13 @@ import rightArrow from '../images/arrow-right-icon.svg';
 import leftArrow from '../images/arrow-left-icon.svg';
 import CustomSwiper from '../utils/Constants/custom-components/CustomSwiper';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggelPopup } from '../features/commonSlice';
 
 const tarndingBanner = [sliderBanner1, sliderBanner2, sliderBanner3, sliderBanner4]
 const LandingContainer = () => {
+    const {isPopup} = useSelector(state=>state.common)
+    const dispatch = useDispatch()
     return (
         <>
             <section className="banner-section text-center">
@@ -17,7 +21,7 @@ const LandingContainer = () => {
                         <div className="swiper banner-slider"> */}
                             <div className="swiper-wrapper swiper-container position-relative">
                                 <CustomSwiper navigationNext={'.banner-button-next'} navigationPrev={'.banner-button-prev'} noOfSlidesPerView={1} isBreakPoint={false}>
-                                    {tarndingBanner.map(bannerUrl => (
+                                    {/* {tarndingBanner.map(bannerUrl => (
                                         <swiper-slide>
                                             <div className="swiper-slide banner1" style={{ backgroundImage: `url(${bannerUrl})` }}>
                                                 <div className="banner-caption">
@@ -31,7 +35,59 @@ const LandingContainer = () => {
                                                 </div>
                                             </div>
                                         </swiper-slide>
-                                    ))}
+                                    ))} */}
+                                    <swiper-slide>
+                                            <div className="swiper-slide banner1" style={{ backgroundImage: `url(${tarndingBanner[0]})` }}>
+                                                <div className="banner-caption">
+                                                    <h1 className="text-center">
+                                                        <span className="d-block">From</span>
+                                                        College Admissions to <strong className="d-block">Being Job Ready</strong>
+                                                    </h1>
+                                                    <div className="banner-btn-grp text-center mt-5 sm:mt-3">
+                                                        <Link className="theme-btn black-btn" onClick={()=>dispatch(toggelPopup({flag:true}))}>Get Started</Link>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </swiper-slide>
+                                        <swiper-slide>
+                                            <div className="swiper-slide banner1" style={{ backgroundImage: `url(${tarndingBanner[1]})` }}>
+                                                <div className="banner-caption">
+                                                    <h1 className="text-center">
+                                                        <span className="d-block">From</span>
+                                                        College Admissions to <strong className="d-block">Being Job Ready</strong>
+                                                    </h1>
+                                                    <div className="banner-btn-grp text-center mt-5 sm:mt-3">
+                                                        <Link className="theme-btn black-btn" onClick={()=>dispatch(toggelPopup({flag:'true'}))}>Get Started</Link>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </swiper-slide>
+                                        <swiper-slide>
+                                            <div className="swiper-slide banner1" style={{ backgroundImage: `url(${tarndingBanner[2]})` }}>
+                                                <div className="banner-caption">
+                                                    <h1 className="text-center">
+                                                        <span className="d-block">From</span>
+                                                        College Admissions to <strong className="d-block">Being Job Ready</strong>
+                                                    </h1>
+                                                    <div className="banner-btn-grp text-center mt-5 sm:mt-3">
+                                                        <Link className="theme-btn black-btn" onClick={()=>dispatch(toggelPopup({flag:'true'}))}>Get Started</Link>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </swiper-slide>
+                                        <swiper-slide>
+                                            <div className="swiper-slide banner1" style={{ backgroundImage: `url(${tarndingBanner[3]})` }}>
+                                                <div className="banner-caption">
+                                                    <h1 className="text-center">
+                                                        <span className="d-block">From</span>
+                                                        College Admissions to <strong className="d-block">Being Job Ready</strong>
+                                                    </h1>
+                                                    <div className="banner-btn-grp text-center mt-5 sm:mt-3">
+                                                        <Link className="theme-btn black-btn" onClick={()=>dispatch(toggelPopup({flag:'true'}))}>Get Started</Link>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </swiper-slide>
                                 </CustomSwiper>
                                 <div className="swiper-button-next banner-button-next">
                                     <img src={rightArrow} alt="" />
